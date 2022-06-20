e9ea58da334be260b281a691d7ff10a44ba02a56
UllrS <94692310+UllrS@users.noreply.github.com>
2022-06-20 22:23:12 +0300

    Update FullAES.swift

--- FullAES.swift
+++ FullAES.swift
@@ -1,50 +1,110 @@
 import Foundation
 import CryptoKit
 
-class CryptAES{
+class CryptoManager{
     
-    var key = SymmetricKey(size: .bits256)
+    var key: SymmetricKey = SymmetricKey(size: .bits256)
+    let sold: String = "965f89884cbc137da047e2d30f9bbcd2633f25c61ff90fa3bcc1a46714089bc0åœ∑m-dv√–œ“´¬œ£-•ˆ®œ“mqverqwp184åß∂ssqΩç"
     
     init(){
-        getRandomKey()
+        setRandomKey()
+        
     }
-    init(_ key: String) {
-        getKey(passwordStr: key)
+    init(keyStr: String) {
+        setKey(passwordStr: keyStr)
     }
-    func getKey(passwordStr: String){
+    init(keyHash: Data){
+        self.key = SymmetricKey(data: keyHash)
+    }
+    init(summetricKey: SymmetricKey){
+        self.key = summetricKey
+    }
+    func changeKey(passwordStr: String) -> Bool{
+        let password = passwordStr.data(using: .utf8)!
+        let hash = self.hashSHA256fromData(data: password)
+        let key = SymmetricKey(data: hash)
+        if self.key == key{
+            self.key = key
+            return true
+        } else {
+            return false
+        }
+    }
+    func eqKey(passwordStr: String) -> Bool{
+        let password = passwordStr.data(using: .utf8)!
+        let hash = self.hashSHA256fromData(data: password)
+        let key = SymmetricKey(data: hash)
+        if self.key == key{
+            return true
+        } else {
+            return false
+        }
+    }
+    func eqKey(passwordHash: Data) -> Bool{
+        let key = SymmetricKey(data: passwordHash)
+        if self.key == key{
+            return true
+        } else {
+            return false
+        }
+    }
+    func setKey(data: Data){
+        self.key = SymmetricKey(data: data)
+    }
+    func setKey(passwordStr: String){
         let password = passwordStr.data(using: .utf8)!
         let hash = self.hashSHA256fromData(data: password)
         self.key = SymmetricKey(data: hash)
     }
-    func getRandomKey(){
+    func getKey(passwordStr: String) -> Data{
+        let password = passwordStr.data(using: .utf8)!
+        let hash = self.hashSHA256fromData(data: password)
+        return hash
+    }
+    func setRandomKey(){
         self.key = SymmetricKey(size: .bits256)
     }
     func encryptString(stringData: String) -> AES.GCM.SealedBox {
-        let data = try! stringData.data(using: .utf8)!
+        let data = stringData.data(using: .utf8)!
         let sealedBox = try! AES.GCM.seal(data, using: self.key)
         return sealedBox
     }
-    func encryptData(data: Data) -> AES.GCM.SealedBox{
+    func encryptData(data: Data) -> Data?{
+        print(self.key)
+        let sealedBox = try! AES.GCM.seal(data, using: self.key, nonce: AES.GCM.Nonce())
+        return sealedBox.combined
+    }
+    func decryptData(data: Data) throws -> Data?{
+        let sealedBox = try! AES.GCM.SealedBox(combined: data)
+        let decryptedData = try? AES.GCM.open(sealedBox, using: self.key)
+        return decryptedData
+    }
+    func encryptSealBox(data: Data) -> AES.GCM.SealedBox{
         let sealedBox = try! AES.GCM.seal(data, using: self.key, nonce: AES.GCM.Nonce())
         return sealedBox
     }
-    func decryptData(sealedBox: AES.GCM.SealedBox) -> Data{
+    func decryptSealBox(sealedBox: AES.GCM.SealedBox) -> Data{
         let sealedBoxRestored = try! AES.GCM.SealedBox(nonce: sealedBox.nonce, ciphertext: sealedBox.ciphertext, tag: sealedBox.tag)
         let decryptedData = try! AES.GCM.open(sealedBoxRestored, using: key)
         return decryptedData
     }
     func HashSHA256fromString(string: String) -> String{
-        let data = Data(string.utf8)
+        let data = Data(salty(password: string).utf8)
         let digest = SHA256.hash(data: data)
         let hash = digest.compactMap{ String(format: "%02x", $0) }.joined()
-        print(hash)
         return hash
     }
     func hashSHA256fromData(data: Data) -> Data{
-        let digest = SHA256.hash(data: data)
+        let digest = SHA256.hash(data: salty(data: data))
         return Data(digest.makeIterator())
     }
     func SHA256DigestToHex(digest: SHA256Digest) -> String{
         return digest.compactMap{ String(format: "%02x", $0) }.joined()
+    }
+    func salty(data: Data) -> Data{
+        return data + self.sold.data(using: .utf8)!
+    }
+    func salty(password: String) -> String{
+        return password + self.sold
     }
 }
